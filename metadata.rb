881ccdc91be237ae4f8ae29e48b7664331a3a415
Vitaly[AVVS] <v@aminev.me>
2013-05-30 18:04:36 +0400

    precise version to regexp

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'v@aminev.me'
 license          'Apache 2.0'
 description      'Installs/Configures rethinkdb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.8'
+version          '0.3.9'
 
 depends 'apt'
